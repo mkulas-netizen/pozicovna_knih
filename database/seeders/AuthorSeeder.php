808f5d918a9f6814407b3c2e89cb18d110b73254
mkulas <mkulas@bjs.sk>
2021-10-01 16:27:00 +0200

    Add seeder my data

--- database/seeders/AuthorSeeder.php
+++ database/seeders/AuthorSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Author;
 use Illuminate\Database\Seeder;
 
 class AuthorSeeder extends Seeder
@@ -13,6 +14,19 @@ class AuthorSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Author::insert([
+            [
+               'name' => 'Silas',
+               'surname' => 'Atkinson'
+            ],
+            [
+                'name' => 'Khadija',
+                'surname' => 'Atherton',
+            ],
+            [
+                'name' => 'Rosa',
+                'surname' => 'Connolly'
+            ]
+        ]);
     }
 }
--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Book;
 use Illuminate\Database\Seeder;
 
 class BookSeeder extends Seeder
@@ -13,6 +14,52 @@ class BookSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Book::insert([
+            [
+           'author_id' => 1,
+            'title' => 'Butcher Of The West',
+            'is_borrowed' => 1
+                ],
+            [
+                'author_id' => 1,
+                'title' => 'Hunter Of Dawn',
+                'is_borrowed' => 1
+            ],
+            [
+                'author_id' => 1,
+                'title' => 'Strangers Without Flaws',
+                'is_borrowed' => 0
+            ],
+            [
+                'author_id' => 2,
+                'title' => 'Slaves And Heirs',
+                'is_borrowed' => 0
+            ],
+            [
+                'author_id' => 2,
+                'title' => 'Enemies And Heirs',
+                'is_borrowed' => 0
+            ],
+            [
+                'author_id' => 2,
+                'title' => 'Concept Of The River',
+                'is_borrowed' => 0
+            ],
+            [
+                'author_id' => 2,
+                'title' => 'Castle Without Direction',
+                'is_borrowed' => 1
+            ],
+            [
+                'author_id' => 3,
+                'title' => 'Vanish At My Destiny',
+                'is_borrowed' => 1
+            ],
+            [
+                'author_id' => 3,
+                'title' => 'Write About The Castle',
+                'is_borrowed' => 0
+            ],
+        ]);
     }
 }
