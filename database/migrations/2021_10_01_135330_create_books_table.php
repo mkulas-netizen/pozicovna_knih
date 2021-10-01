80cfb0dcc9ecd1f42bbdd735fc0b9950e6fe6c87
mkulas <mkulas@bjs.sk>
2021-10-01 16:06:12 +0200

    Create table columnt and relocation default model and migration

---
+++ database/migrations/2021_10_01_135330_create_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->integer('author_id');
+            $table->string('title',255);
+            $table->boolean('is_borrowed')->comment('1 -> is borrowed , 0 - not borrowed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
